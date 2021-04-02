d4001bbcd57e63f1bc389f0ed82b84d62a043bbb
JanjanC <janjan_chua@dlsu.edu.ph>
2021-04-03 03:49:58 +0800

    Fixed parameter name

--- source-code/profile.js
+++ source-code/profile.js
@@ -1,6 +1,6 @@
 function updateFollow (ID) {
 
-    var user = document.getElementById(userID);
+    var user = document.getElementById(ID);
     var follow = user.getElementsByClassName("follow")[0];
     var status = follow.getElementsByTagName("span")[0];
 
