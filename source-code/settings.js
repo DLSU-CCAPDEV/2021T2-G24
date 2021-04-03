56850bd455a23346d03faef84e65628741720360
JanjanC <janjan_chua@dlsu.edu.ph>
2021-04-04 00:19:01 +0800

    Updated to correct version

--- source-code/settings.js
+++ source-code/settings.js
@@ -9,6 +9,7 @@ function addFav () {
     var input = fav.getElementsByTagName("input")[0].value;
 
     var favWorksDiv = document.getElementById("fav-works");
+
     // div
     var div = document.createElement("div");
     div.classList.add("d-flex", "border-bottom", "align-items-center", "justify-content-between", "fav-works"); //class
@@ -16,11 +17,13 @@ function addFav () {
     var id = document.createAttribute("id");
     id.value = "" + favWorks.length;
     div.setAttributeNode(id);
+
     // h6
     var h6 = document.createElement("h6");
     h6.classList.add("m-3");
     h6.innerHTML = input;
     div.appendChild(h6);
+
     //button
     var button = document.createElement("button");
     var type = document.createAttribute("type"); //type
@@ -34,6 +37,7 @@ function addFav () {
     i.classList.add("fa", "fa-times")
     button.appendChild(i);
     div.appendChild(button);
+
     favWorksDiv.insertBefore(div, favWorks[favWorks.length - 1]);
     fav.getElementsByTagName("input")[0].value = "";
 }
