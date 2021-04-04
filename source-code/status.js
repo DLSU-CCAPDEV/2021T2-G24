3fa8e9f9f9a4003725525c76a3d161cb45b941a8
JanjanC <janjan_chua@dlsu.edu.ph>
2021-04-04 23:52:01 +0800

    Changed default user

--- source-code/status.js
+++ source-code/status.js
@@ -10,7 +10,7 @@ function checkStatus () {
         `
         <a class="navbar-brand mr-auto" href="index.html">
             <img src="../images/logo.png" alt="logo" style="width: 40px">
-            <span>Writer's Kiln</span>
+            <h3 class="d-inline align-middle" id="navbar-name">Writer's Kiln</h3>
         </a>
 
         <form class="form-inline ml-auto mr-auto" action="search-results.html" method="post">
@@ -35,11 +35,11 @@ function checkStatus () {
             <li class="nav-item">
                 <div class="btn-group ml-auto">
                     <button type="button" class="btn btn-warning rounded ml-3" data-toggle="dropdown">
-                        <span><i class="fa fa-user-circle-o"></i> User Name</span>
+                        <span><i class="fa fa-user-circle-o"></i> H.P.Lovecraft</span>
                     </button>
                     <div class="dropdown-menu dropdown-menu-right">
                         <a class="dropdown-item" href="profile.html">My Profile</a>
-                        <a class="dropdown-item" href="settings.html">Settings</a>
+                        <a class="dropdown-item" href="settings.html">Profile Settings</a>
                         <a class="dropdown-item" href="index.html" onclick="signOut()">Sign Out</a>
                     </div>
                 </div>
@@ -48,10 +48,10 @@ function checkStatus () {
         `;
     } else {
         document.getElementsByTagName("nav")[0].innerHTML =
-       `
-       <a class="navbar-brand mr-auto" href="index.html">
+        `
+        <a class="navbar-brand mr-auto" href="index.html">
             <img src="../images/logo.png" alt="logo" style="width: 40px">
-            <span>Writer's Kiln</span>
+            <h3 class="d-inline align-middle" id="navbar-name">Writer's Kiln</h3>
         </a>
 
         <form class="form-inline ml-auto mr-auto" action="search-results.html" method="post">
