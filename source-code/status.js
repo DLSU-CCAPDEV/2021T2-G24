ba9a6d25e844bd9b6cd890c2bf2373d25e8d7710
JanjanC <janjan_chua@dlsu.edu.ph>
2021-04-05 05:06:16 +0800

    Restricted visitor's possible operations

--- source-code/status.js
+++ source-code/status.js
@@ -4,7 +4,7 @@ function checkStatus () {
         localStorage.setItem("STATUS", "false");
     }
 
-    if (localStorage.getItem("STATUS") == "true") {
+    if (localStorage.getItem("STATUS") == "true") { //signed in
 
         document.getElementsByTagName("nav")[0].innerHTML =
         `
@@ -89,4 +89,83 @@ function signIn () {
 
 function signOut () {
     localStorage.setItem("STATUS", "false");
+}
+
+function updateClickables () {
+
+    if (localStorage.getItem("STATUS") == "false") { //visitor
+        //change functionality of upvote
+        var upvote = document.getElementsByClassName("upvote");
+        for (var i = 0; i < upvote.length; i++) {
+            upvote[i].removeAttribute("onclick");
+            upvote[i].setAttribute("data-toggle", "modal");
+            upvote[i].setAttribute("data-target", "#visitorModal");
+        }
+
+        //change functionality of downvote
+        var downvote = document.getElementsByClassName("downvote");
+        for (var i = 0; i < downvote.length; i++) {
+            downvote[i].removeAttribute("onclick");
+            downvote[i].setAttribute("data-toggle", "modal");
+            downvote[i].setAttribute("data-target", "#visitorModal");
+        }
+
+        //change functionality of create buttons
+        var create = document.getElementsByClassName("create");
+        for (var i = 0; i < create.length; i++) {
+            create[i].removeAttribute("onclick");
+            create[i].setAttribute("data-toggle", "modal");
+            create[i].setAttribute("data-target", "#visitorModal");
+        }
+
+        var follow = document.getElementsByClassName("follow");
+        for (var i = 0; i < follow.length; i++) {
+            follow[i].removeAttribute("onclick");
+            follow[i].setAttribute("data-toggle", "modal");
+            follow[i].setAttribute("data-target", "#visitorModal");
+
+            follow[i].classList.remove("btn-warning");
+            follow[i].classList.add("btn-outline-warning");
+            follow[i].innerText = "Follow";
+        }
+
+
+        //modal
+        var div = document.createElement("div");
+        div.classList.add("modal", "fade");
+        div.setAttribute("id", "visitorModal");
+        div.innerHTML =
+        `
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Notice</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+
+                <!-- Modal body -->
+                <div class="modal-body">
+                    You must sign in first before performing this operation.
+                </div>
+
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+                </div>
+
+            </div>
+        </div>
+        `;
+
+        document.getElementsByTagName("body")[0].appendChild(div);
+
+        var modify = document.getElementsByClassName("modify");
+
+        while (modify.length > 0) {
+            modify[0].remove();
+        }
+    }
+
 }
