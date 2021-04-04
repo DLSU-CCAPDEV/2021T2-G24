9edfacf025357db0f97c26f819dd61a310eb04da
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-04-05 00:31:01 +0800

    Changed design and layout

--- source-code/search-results.js
+++ source-code/search-results.js
@@ -64,18 +64,17 @@ function updateDownvote (postID) {
 }
 
 function updateFollow (ID) {
-    alert("Hi");
     var user = document.getElementById(ID);
     var follow = user.getElementsByClassName("follow")[0];
     var status = follow.getElementsByTagName("span")[0];
 
-    if (status.innerHTML == "Follow") { //follow
-        status.innerHTML = "Following";
-        follow.classList.remove("btn-dark");
-        follow.classList.add("btn-outline-dark");
+    if (status.innerHTML == "<strong>Follow</strong>") { //follow
+        status.innerHTML = "<strong>Following</strong>";
+        follow.classList.remove("btn-outline-warning");
+        follow.classList.add("btn-warning");
     } else { //unfollow
-        status.innerHTML = "Follow";
-        follow.classList.remove("btn-outline-dark");
-        follow.classList.add("btn-dark");
+        status.innerHTML = "<strong>Follow</strong>";
+        follow.classList.remove("btn-warning");
+        follow.classList.add("btn-outline-warning");
     }
 }
