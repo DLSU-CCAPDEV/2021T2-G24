45d04dd0bffd97911fe8c827c31b464e76fcc02e
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-04-27 07:40:45 +0800

    Add hbs module and path, commented out db, added tester for profile

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const dotenv = require(`dotenv`);
 const fs = require(`fs`);
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
-const db = require(`./model/db.js`);
+const hbs = require(`hbs`);
+const path = require('path');
+//const db = require(`./models/db.js`);
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.set(`view engine`, `hbs`);
+app.use(express.static(path.join(__dirname, '/public')));
 
 dotenv.config();
 port = process.env.PORT;
@@ -23,6 +27,25 @@ app.get(`/`, function(req, res) {
 		}
 		res.end();
 	});
+});
+
+//colon after represent it as being a parameter
+app.get(`/profile/:username`, function(req, res){
+    //get the values from the database
+    //For now we're hardcoding for simulation purposes
+    /*
+    var person = {
+        fn: `Ned`,//should be from database
+        ln: `Stark`//should be from database
+    };
+    */
+
+    var person = {
+        username: req.params.username,
+		realname: `Howard Philips`
+    };
+    //first parameter is the rendering of file, succeeding are objects
+    res.render(`profile`, person);
 });
 
 app.listen(port, hostname, function() {
