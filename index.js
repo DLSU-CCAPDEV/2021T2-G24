07bdc4430afc76c902c2c5812bfa9ca959967d90
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-04-30 07:14:32 +0800

    Add formatLineBreak and formatDate helper functions

--- index.js
+++ index.js
@@ -17,7 +17,21 @@ dotenv.config();
 port = process.env.PORT;
 hostname = process.env.HOSTNAME;
 
+hbs.registerHelper('formatLineBreak', function(textContent) {
+    var newString = textContent.replace('\n', '<br>');
+    return newString;
+});
+
+hbs.registerHelper('formatDate', function(date) {
+    var newDate = new Date(date)
+	var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][newDate.getMonth()];
+	var day = newDate.getDate();
+	var year = newDate.getFullYear();
+	var newString = month + ' ' + day + ', ' + year;
+    return newString;
+});
+
 app.listen(port, hostname, function() {
 	console.log(`Server running at:`);
 	console.log(`http://` + hostname + `:` + port);
-})
+});
