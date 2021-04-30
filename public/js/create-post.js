569dabc14131f2bd0f348c69e674ccf4176d4c44
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-04-30 15:48:16 +0800

    Add requirement for fields

--- public/js/create-post.js
+++ public/js/create-post.js
@@ -15,6 +15,120 @@ function clicked(tab) {
     }
 }
 
+/*
+var username = document.getElementById("username");
+if(username.value.trim() == "") {
+    error.innerText = "Please enter your destined username."
+    return false;
+} else {
+    error.innerText = ""
+    return true;
+}
+*/
+
+function checkGeneral() {
+    var error = document.getElementById("error");
+    var content = document.getElementById("genContent");
+    if(content.value.trim() == "") {
+        error.innerText = "Please enter a content for the general field.";
+        return false;
+    } else {
+        error.innerText = "";
+        return true;
+    }
+}
+
+function checkStory() {
+    var error = document.getElementById("error");
+    var plot = document.getElementById("plotContent");
+    var char = document.getElementById("charContent");
+    var setting = document.getElementById("settingContent");
+
+    function checkPlot() {
+        if(plot.value.trim() == "") {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function checkSetting() {
+        if(setting.value.trim() == "") {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function checkCharacters() {
+        if(char.value.trim() == "") {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    if(checkPlot() || checkSetting() || checkCharacters()) {
+        error.innerText = "";
+        return true;
+    } else {
+        error.innerText = "Please enter content to at least one of the fields.";
+        return false;
+    }
+}
+
+function checkMedia() {
+    var error = document.getElementById("error");
+    var content = document.getElementById("thumbnail");
+    if(content.value == "") {
+        error.innerText = "Please upload a media content.";
+        return false;
+    } else {
+        error.innerText = "";
+        return true;
+    }
+}
+
+function checkTags() {
+    var error = document.getElementById("error");
+    var tag = document.getElementById("tags");
+    if(tag.value.trim() == "") {
+        error.innerText = "Please include at least one tag.";
+        return false;
+    } else {
+        error.innerText = "";
+        return true;
+    }
+}
+
+function checkTitle() {
+    var error = document.getElementById("error");
+    var title = document.getElementById("title");
+    if(title.value.trim() == "") {
+        error.innerText = "Please include a post title.";
+        return false;
+    } else {
+        error.innerText = "";
+        return true;
+    }
+}
+
+function checkSubmit() {
+    var tab = document.getElementsByClassName("nav-link active")[0].id;
+    if(checkTags() && checkTitle()) {
+        switch(tab) {
+            case "general-tab":
+                return checkGeneral();
+            case "story-tab":
+                return checkStory();
+            case "media-tab":
+                return checkMedia();
+        }
+    } else {
+        return false;
+    }
+}
+
 function clearStory() {
     document.getElementById("plotContent").value = "";
     document.getElementById("charContent").value = "";
