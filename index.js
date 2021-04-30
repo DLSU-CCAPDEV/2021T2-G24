f0b270f5a2d9fbcf6921242b03204bea997423e7
JanjanC <janjan_chua@dlsu.edu.ph>
2021-04-30 19:55:54 +0800

    Placed handlebars helper to a separate file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const dotenv = require(`dotenv`);
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
 const hbs = require(`hbs`);
+const handlebars_helper = require(`./views/handlebars-helper.js`);
 const routes = require(`./routes/routes.js`);
 const session = require('express-session');
 
@@ -16,20 +17,6 @@ app.use(`/`, routes);
 dotenv.config();
 port = process.env.PORT;
 hostname = process.env.HOSTNAME;
-
-hbs.registerHelper('formatLineBreak', function(textContent) {
-    var newString = textContent.replace('\n', '<br>');
-    return newString;
-});
-
-hbs.registerHelper('formatDate', function(date) {
-    var newDate = new Date(date)
-	var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][newDate.getMonth()];
-	var day = newDate.getDate();
-	var year = newDate.getFullYear();
-	var newString = month + ' ' + day + ', ' + year;
-    return newString;
-});
 
 app.listen(port, hostname, function() {
 	console.log(`Server running at:`);
