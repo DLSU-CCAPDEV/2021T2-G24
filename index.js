85587a4841fe7b578541ae07a8db070f7f9a8ef8
JanjanC <janjan_chua@dlsu.edu.ph>
2021-05-17 02:51:31 +0800

    Updated to server-side session storage

--- index.js
+++ index.js
@@ -14,8 +14,14 @@ app.set(`view engine`, `hbs`);
 hbs.registerPartials(__dirname + `/views/partials`);
 
 app.use(express.static(`public`));
-app.use(session({secret: `writers-kiln-session`, resave: false, saveUninitialized: false}));
 app.use(`/`, routes);
+
+app.use(session({
+    secret: `writers-kiln-session`,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
 
 dotenv.config();
 port = process.env.PORT;
