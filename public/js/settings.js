505b882845e7cf9945b4122438d0d732fcd48da7
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-05-20 01:53:43 +0800

    Removed feeding username to the query and added removeFeat

--- public/js/settings.js
+++ public/js/settings.js
@@ -13,38 +13,27 @@ function checkFavDupe(title) {
 function removeFav (favId, favTitle) {
     //Div
     var fav = document.getElementById(favId);
-    var username = document.getElementById("username").value;
     var title = favTitle.slice(10);
 
-    var update = {
-        username: username,
-        title: title
-    }
-
-    $.get('/delete-favorite', update, function(result) {
+    $.get('/delete-favorite', {title: title}, function(result) {
         fav.remove();
     });
 }
 
 function addFav() {
     var fav = $("#newFav").val().trim();
-    var username = $("#username").val();
     var error = $("#error");
 
     //if empty
     if(fav == "") {
         error.html("Please fill up a valid title");
     } else {
-        var update = {
-            username: username,
-            favorite_work: fav
-        }
         //if there's a duplicate
         if(checkFavDupe(fav)) {
             error.html("An instance of work with this title already exist in your list");
         } else {
             error.html("");
-            $.get('/add-favorite', update, function(result) {
+            $.get('/add-favorite', {favorite_work: fav}, function(result) {
                 /**********THE CHANGE IN THE FRONT END**********/
                 var fav = document.getElementById("fav-add");
                 var input = fav.getElementsByTagName("input")[0].value;
@@ -92,6 +81,11 @@ function addFav() {
 }
 
 function removeFeat(featID) {
+    //Div
     var feat = document.getElementById(featID);
-    feat.remove();
+    var title = featID.slice(5);
+
+    $.get('/delete-featured', {title: title}, function(result) {
+        feat.remove();
+    });
 }
