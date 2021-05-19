dac68110434a8e8a3224362a34d82b49c640219e
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-05-19 22:07:34 +0800

    Add validator

--- public/js/create-featured-work.js
+++ public/js/create-featured-work.js
@@ -1,3 +1,23 @@
+$(document).ready(function () {
+    $('#title').keyup(function () {
+        var title = document.getElementById("title").value.trim();
+        var oldtitle = document.getElementById("old-title").innerText.trim();
+
+        if(title != oldtitle) {
+            $.get('/get-check-featured-work', {title : title}, function(result) {
+                //If a dupe was found
+                if(result) {
+                    $('#error').text('An instance with this title already exist your list');
+                    $('#submit').prop('disabled', true);
+                } else {
+                    $('#error').text('');
+                    $('#submit').prop('disabled', false);
+                }
+            });
+        }
+    });
+});
+
 function checkTitle() {
     var error = document.getElementById("error");
     var title = document.getElementById("title");
