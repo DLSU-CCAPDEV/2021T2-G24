eeaa89ee5356e909e4dab83096986814597db4d5
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-05-24 11:31:35 +0800

    addValidator to settings

--- controllers/settings-controller.js
+++ controllers/settings-controller.js
@@ -3,7 +3,7 @@ const User = require(`../models/user-model.js`);
 const ObjectId = require(`mongodb`).ObjectID;
 const bcrypt = require(`bcryptjs`);
 const saltRounds = 10;
-
+const { validationResult } = require('express-validator');
 const multer = require(`multer`);
 const path = require('path');
 const fs = require(`fs`);
@@ -44,61 +44,78 @@ const settingsController = {
     },
 
     postProfileSettings: function(req, res) {
-        var fullname = req.body.fullname;
-        var email = req.body.email;
-        var username = req.body.username;
-        var password = req.body.newpassword;
-        var profile_picture = req.file;
-        console.log(req.body.avatar);
-        console.log(req.file);
-        var about_me = req.body.about;
-        var privacy = req.body.privacy;
+        // fetches validation errors
+        var errors = validationResult(req);
 
-        if(privacy == "true") {
-            privacy = true;
+        // if there are validation errors
+        if (!errors.isEmpty()) {
+
+            // get the array of errors
+            errors = errors.errors;
+
+            var details = {};
+
+            if(errors.length > 0)
+            details['error-pass'] = errors[0].msg
+
+            res.render('settings', details);
         } else {
-            privacy = false;
-        }
+            var fullname = req.body.fullname;
+            var email = req.body.email;
+            var username = req.body.username;
+            var password = req.body.newpassword;
+            var profile_picture = req.file;
+            console.log(req.body.avatar);
+            console.log(req.file);
+            var about_me = req.body.about;
+            var privacy = req.body.privacy;
 
-        db.findOne(User, {_id: new ObjectId(req.session.userID)}, function(result) {
-            bcrypt.hash(password, saltRounds, function(err, hash) {
-                if(password == "") {
-                    password = result.password;
-                } else {
-                    password = hash;
-                }
+            if(privacy == "true") {
+                privacy = true;
+            } else {
+                privacy = false;
+            }
 
-                if (profile_picture == null) {
-                    profile_picture = result.profile_picture;
-                } else {
-                    var img = fs.readFileSync(req.file.path);
-                    var encode_image = img.toString('base64');
-
-                    profile_picture = {
-                        data: Buffer.from(encode_image, 'base64'),
-                        contentType: req.file.mimetype
+            db.findOne(User, {_id: new ObjectId(req.session.userID)}, function(result) {
+                bcrypt.hash(password, saltRounds, function(err, hash) {
+                    if(password == "") {
+                        password = result.password;
+                    } else {
+                        password = hash;
                     }
-                }
 
-                var update = {
-                    $set: { fullname: fullname,
+                    if (profile_picture == null) {
+                        profile_picture = result.profile_picture;
+                    } else {
+                        var img = fs.readFileSync(req.file.path);
+                        var encode_image = img.toString('base64');
+
+                        profile_picture = {
+                            data: Buffer.from(encode_image, 'base64'),
+                            contentType: req.file.mimetype
+                        }
+                    }
+
+                    var update = {
+                        $set: { fullname: fullname,
                             email: email,
                             username: username,
                             password: password,
                             profile_picture: profile_picture,
                             about_me: about_me,
                             privacy: privacy
+                        }
                     }
-                }
 
-                db.updateOne(User, {username: req.session.username}, update, function(result) {
+                    db.updateOne(User, {username: req.session.username}, update, function(result) {
                         if(result) {
                             req.session.username = req.body.username;
                             res.redirect(`/profile/` + username);
                         }
                     });
+                });
             });
-        });
+        }
     },
 
     getCheckFeaturedWork: function(req, res) {
@@ -189,11 +206,11 @@ const settingsController = {
 
         console.log(`PARAMSTITLE IS : ` + req.params.title)
         db.updateOne(User, {username: req.session.username, "featured_works.title" : req.params.title},
-                {$set: {"featured_works.$.title" : req.body.title,
-                "featured_works.$.synopsis" : req.body.synopsis,
-                "featured_works.$.url" : req.body.link,
-                "featured_works.$.thumbnail" : req.body.thumbnail}}, function(){
-                res.redirect(`/settings/`);
+        {$set: {"featured_works.$.title" : req.body.title,
+        "featured_works.$.synopsis" : req.body.synopsis,
+        "featured_works.$.url" : req.body.link,
+        "featured_works.$.thumbnail" : req.body.thumbnail}}, function(){
+            res.redirect(`/settings/`);
         });
     },
 
