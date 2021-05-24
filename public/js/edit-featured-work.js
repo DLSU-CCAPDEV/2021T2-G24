3d34a0bc6d55c785c85e99e5a9cd868e3ac9fb77
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-05-24 23:32:53 +0800

    Fixed edit and create featured works

--- public/js/edit-featured-work.js
+++ public/js/edit-featured-work.js
@@ -1,18 +1,20 @@
 $(document).ready(function () {
     $('#title').keyup(function () {
         var title = document.getElementById("title").value.trim();
+        var oldtitle = document.getElementById("old-title").innerText.trim();
 
-        $.get('/get-check-featured-work', {title : title}, function(result) {
-            //If a dupe was found
-            if(result) {
-                $('#error').text('An instance with this title already exist your list');
-                $('#submit').prop('disabled', true);
-            } else {
-                $('#error').text('');
-                $('#submit').prop('disabled', false);
-            }
-        }, ``);
-
+        if(title != oldtitle) {
+            $.get('/get-check-featured-work', {title : title}, function(result) {
+                //If a dupe was found
+                if(result) {
+                    $('#error').text('An instance with this title already exist your list');
+                    $('#submit').prop('disabled', true);
+                } else {
+                    $('#error').text('');
+                    $('#submit').prop('disabled', false);
+                }
+            }, ``);
+        }
     });
 });
 
