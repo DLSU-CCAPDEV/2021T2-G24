97bb502c57be9e7e985176dcdc1342bdb31e6016
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-05-24 13:01:46 +0800

    Fixed period format, removed pass for setting,  and added featured Validation

--- helpers/validation.js
+++ helpers/validation.js
@@ -11,19 +11,19 @@ const validation = {
 
         var validation = [
             // checks if `fullname` is not empty
-            check('fullname', 'Fullname should not be empty.').notEmpty(),
+            check('fullname', 'Fullname should not be empty').notEmpty(),
 
             // checks if `email` is not empty
-            check('email', 'Email should not be empty.').notEmpty(),
+            check('email', 'Email should not be empty').notEmpty(),
 
             // checks if `email` is a valid email address
-            check('email', 'A valid email address is required.').isEmail(),
+            check('email', 'A valid email address is required').isEmail(),
 
             // checks if `username` is not empty
-            check('username', 'Username should not be empty.').notEmpty(),
+            check('username', 'Username should not be empty').notEmpty(),
 
             // checks if `password` is not empty
-            check('password', 'Password should not be empty.').notEmpty()
+            check('password', 'Password should not be empty').notEmpty()
         ];
 
         return validation;
@@ -32,7 +32,7 @@ const validation = {
     commentValidation: function() {
         var validation = [
                 //checks if `comment` is not empty
-                check('comment', 'Comment should not be empty.').notEmpty()
+                check('comment', 'Comment should not be empty').notEmpty()
         ];
 
         return validation;
@@ -41,19 +41,34 @@ const validation = {
     settingsValidation: function() {
         var validation = [
             // checks if `fullname` is not empty
-            check('fullname', 'Fullname should not be empty.').notEmpty(),
+            check('fullname', 'Fullname should not be empty').notEmpty(),
 
             // checks if `email` is not empty
-            check('email', 'Email should not be empty.').notEmpty(),
+            check('email', 'Email should not be empty').notEmpty(),
 
             // checks if `email` is a valid email address
-            check('email', 'A valid email address is required.').isEmail(),
+            check('email', 'A valid email address is required').isEmail(),
 
             // checks if `username` is not empty
-            check('username', 'Username should not be empty.').notEmpty(),
+            check('username', 'Username should not be empty').notEmpty(),
+        ];
 
-            // checks if `password` is not empty
-            check('password', 'Password should not be empty.').notEmpty()
+        return validation;
+    },
+
+    featuredValidation: function() {
+        var validation = [
+            // checks if `title` is not empty
+            check('title', 'Title should not be empty').notEmpty(),
+
+            // checks if `synopsis` is not empty
+            check('synopsis', 'Synopsis should not be empty').notEmpty(),
+
+            // checks if `link` is not empty
+            check('link', 'Link should not be empty').notEmpty(),
+
+            // checks if `link` is a valid URL
+            check('link', 'A valid link is required').isURL(),
         ];
 
         return validation;
